cd2b3b745a1da1e8746907ed1a00146902358e00
Tanya <t.baletska@gmail.com>
2026-02-04 23:57:03 +0200

    change config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/test-task-devhive",
-  assetPrefix: "/test-task-devhive/",
 };
 
 export default nextConfig;
